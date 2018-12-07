6711e5e416211a23ac84b1f2cfe9ad4ae1745b0c
alnolte <31218518+alnolte@users.noreply.github.com>
2018-12-06 19:01:47 -0700

    Update Pacman.py

--- Pacman.py
+++ Pacman.py
@@ -4,6 +4,9 @@ class Pacman:
         self.location = (5,1)
         self.maze = Maze()
 
+    def getLocation(self):
+        return self.location
+
     def actions(self):
         walls = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),
                  (1,0),(2,0),(3,0),(4,0),(5,0),(6,0),
