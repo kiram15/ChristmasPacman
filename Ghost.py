3cac06d178f5d3c49aceee4653a1bb4afed3f412
alnolte <31218518+alnolte@users.noreply.github.com>
2018-12-06 19:02:09 -0700

    Update Ghost.py

--- Ghost.py
+++ Ghost.py
@@ -5,6 +5,9 @@ class Ghost:
         self.coveringDot = False
         self.maze = Maze()
 
+    def getLocation(self):
+        return self.location
+
     def actions(self):
         walls = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
                  (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0),
