68e47d219e25dc7b9f1c88b9f0df481263993a2c
alnolte <31218518+alnolte@users.noreply.github.com>
2018-12-06 18:08:30 -0700

    Create Pacman.py

---
+++ Pacman.py
@@ -0,0 +1,87 @@
+#grinch
+class Pacman:
+    def __init__(self, maze):
+        self.location = (5,1)
+        self.maze = maze()
+
+    def actions(self):
+        walls = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),
+                 (1,0),(2,0),(3,0),(4,0),(5,0),(6,0),
+                 (6,1),(6,2),(6,3),(6,4),(6,5),(6,6),
+                 (1,6),(2,6),(3,6),(4,6),(5,6),
+                 (2,2),(3,2),(4,2),
+                 (2,4),(3,4),(4,4)]
+        validActions = []
+        currentLocation = self.location
+        right = (currentLocation[0] + 1, currentLocation[1])
+        left = (currentLocation[0] - 1, currentLocation[1])
+        up = (currentLocation[0], currentLocation[1] + 1)
+        down = (currentLocation[0], currentLocation[1] - 1)
+        if right not in walls:
+            validActions.append("right")
+        if left not in walls:
+            validActions.append("left")
+        if up not in walls:
+            validActions.append("up")
+        if down not in walls:
+            validActions.append("down")
+        return validActions
+
+    def takeAction(self, action):
+        tLocation = self.location
+        newLocation = ()
+        if action == "right":
+            newLocation = (tLocation[0] + 1, tLocation[1])
+        elif action == "left":
+            newLocation = (tLocation[0] - 1, tLocation[1])
+        elif action == "up":
+            newLocation = (tLocation[0], tLocation[1] + 1)
+        elif action == "down":
+            newLocation = (tLocation[0], tLocation[1] - 1)
+        self.maze.makeMove(self, newLocation[0], newLocation[1])
+        self.location = newLocation
+
+    def takeShortest(self, maze):
+        possibleActions = Pacman.actions(self)
+        shortestDistance = 100
+        bestAction = ""
+        for a in possibleActions:
+            tempDistance = Pacman.testAction(self, a, maze) #distance to closest dot
+            if shortestDistance > tempDistance:
+                shortestDistance = tempDistance
+                bestAction = a
+        Pacman.takeAction(self, bestAction)
+
+    def testAction(self, action, maze): #specified action, which dot is closest
+        tempLocation = self.location
+        if action == "right":
+            newX = tempLocation[0] + 1
+            tempLocation = (newX, tempLocation[1])
+        elif action == "left":
+            newX = tempLocation[0] - 1
+            tempLocation = (newX, tempLocation[1])
+        elif action == "up":
+            newY = tempLocation[1] + 1
+            tempLocation = (tempLocation[0], newY)
+        elif action == "down":
+            newY = tempLocation[1] - 1
+            tempLocation = (tempLocation[0], newY)
+        allDots = Pacman.getDots(maze)
+        minDistance = 100
+        for d in allDots:
+            testDistance = Pacman.distance(self, tempLocation, d)
+            if testDistance < minDistance:
+                minDistance = testDistance
+        return minDistance
+
+    def getDots(self):
+        myMaze = self.maze.maze
+        dotLocations = []
+        for r in range(len(myMaze)):
+            for el in range(len(myMaze[0])):
+                if myMaze[r][el] == '.':
+                    dotLocations.append((r, el))
+        return dotLocations
+
+    def distance(self, l1, l2):
+        return abs(l1[0] - l2[0]) + abs(l1[1] - l2[1])
