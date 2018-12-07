954eaf70a2d38977e4d4adaea9c4c410846bc75c
alnolte <31218518+alnolte@users.noreply.github.com>
2018-12-06 18:09:20 -0700

    Update Dot.py

--- Dot.py
+++ Dot.py
@@ -1,6 +1,6 @@
-class Dot(object):
-    def __init__(self, x, y):
-        self.location = x, y
+# class Dot(object):
+#     def __init__(self, x, y):
+#         self.location = x, y
 
-    def location(self):
-        return self.location
+#     def location(self):
+#         return self.location
